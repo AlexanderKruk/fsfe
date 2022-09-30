721d142230651817d0ee4e214fb110010d2d0871
Alexander Kruk <alexander.kruk@gmail.com>
2022-09-30 15:21:42 +0200

    added status and header

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.get('/',(req, res) => {
 	res.send('Hello world!')
+	res.status(418)
+	res.set({ 'X-my-header': 'my header info' })
 });
 
 app.listen(port,() => console.log(`Example app listening on port ${port}!`));
