346ef71de464434c563c52223083737d8d52c251
Alexander Kruk <alexander.kruk@gmail.com>
2022-09-30 15:27:43 +0200

    move send to end

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req, res) => {
-	res.send('Hello world!!!')
 	res.status(418)
 	res.set({ 'X-my-header': 'my header info' })
+	res.send('Hello world!!!')
 });
 
 app.listen(port,() => console.log(`Example app listening on port ${port}!`));
